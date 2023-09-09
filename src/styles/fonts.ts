d6485ce1700f0130727e06a23c4a628bef419a4a
asr <asr12211@outlook.com>
2023-09-09 18:08:05 +0300

    ADD: fonts file created

---
+++ src/styles/fonts.ts
@@ -0,0 +1,4 @@
+import { Oregano, Laila } from 'next/font/google';
+
+export const oregano = Oregano({ subsets: ['latin'], weight: "400" });
+export const laila = Laila({ subsets: ['latin'], weight: ["400", "600"] });
