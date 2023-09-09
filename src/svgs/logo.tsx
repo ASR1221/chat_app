70d16c058e39af80925a44b54321e406e72be3bc
asr <asr12211@outlook.com>
2023-09-09 17:31:06 +0300

    FIX: svgs fixed & comps made to apply dark mode

---
+++ src/svgs/logo.tsx
@@ -0,0 +1,88 @@
+function Logo({ isDark }: { isDark: boolean }) {
+   
+   return <svg
+      width="9.7201252mm"
+      height="9.682971mm"
+      viewBox="0 0 9.7201252 9.682971"
+      version="1.1"
+      id="svg5"
+      xmlns="http://www.w3.org/2000/svg">
+      <defs id="defs2">
+         <marker
+            style={{overflow: "visible"}}
+            id="TriangleInL"
+            refX="0"
+            refY="0"
+            orient="auto">
+               <path
+                  transform="scale(-0.8)"
+                  style={{fill: "context-stroke", fillRule: "evenodd", stroke: "context-stroke", strokeWidth: "1pt"}}
+                  d="M 5.77,0 -2.88,5 V -5 Z"
+                  id="path1025" />
+         </marker>
+         <marker
+            style={{overflow: "visible"}}
+            id="DiamondL"
+            refX="0"
+            refY="0"
+            orient="auto">
+               <path
+                  transform="scale(0.8)"
+                  style={{fill: "context-stroke", fillRule: "evenodd", stroke: "context-stroke", strokeWidth: "1pt"}}
+                  d="M 0,-7.0710768 -7.0710894,0 0,7.0710589 7.0710462,0 Z"
+                  id="path971" />
+         </marker>
+         <marker
+            style={{overflow: "visible"}}
+            id="Arrow1Mstart"
+            refX="0"
+            refY="0"
+            orient="auto">
+               <path
+                  transform="matrix(0.4,0,0,0.4,4,0)"
+                  style={{fill: "context-stroke", fillRule: "evenodd", stroke: "context-stroke", strokeWidth: "1pt"}}
+                  d="M 0,0 5,-5 -12.5,0 5,5 Z"
+                  id="path898" />
+         </marker>
+         <marker
+            style={{overflow: "visible"}}
+            id="Arrow1Lstart"
+            refX="0"
+            refY="0"
+            orient="auto">
+               <path
+                  transform="matrix(0.8,0,0,0.8,10,0)"
+                  style={{fill: "context-stroke", fillRule: "evenodd", stroke: "context-stroke", strokeWidth: "1pt"}}
+                  d="M 0,0 5,-5 -12.5,0 5,5 Z"
+                  id="path892" />
+         </marker>
+     </defs>
+     <g
+        id="layer2"
+        transform="translate(-43.872969,-206.87077)">
+       <path
+          style={{color: isDark ? "#ffffff" : "#000000", fill: isDark ? "#ffffff" : "#000000", strokeLinecap: "round"}}
+          d="m 50.234375,206.96289 c -0.775982,-0.16195 -1.594206,-0.10866 -2.34375,0.14648 -0.749448,0.25512 -1.427372,0.70992 -1.955078,1.29883 -0.563142,0.62846 -0.955045,1.41434 -1.083985,2.25196 -0.129025,0.83817 0.01274,1.72033 0.429688,2.46289 0.423542,0.75429 1.124099,1.34377 1.939453,1.63281 0.815354,0.28904 1.731096,0.27224 2.535156,-0.0469 a 0.1322915,0.1322915 0 0 0 0.07422,-0.17187 0.1322915,0.1322915 0 0 0 -0.171875,-0.0742 c -0.742758,0.29478 -1.596418,0.30997 -2.349609,0.043 -0.753191,-0.267 -1.405624,-0.81689 -1.796875,-1.51367 -0.385511,-0.68657 -0.518778,-1.51122 -0.398438,-2.29297 0.120426,-0.78231 0.488896,-1.52305 1.019531,-2.11524 0.497544,-0.55525 1.138838,-0.98465 1.84375,-1.22461 0.704816,-0.23992 1.475197,-0.29059 2.203125,-0.13867 0.727821,0.1519 1.41363,0.50916 1.951172,1.02149 0.537457,0.51224 0.927169,1.18065 1.097657,1.90234 0.250822,1.06175 0.01502,2.22826 -0.597657,3.13477 -0.419332,0.62043 -1.005121,1.12539 -1.65625,1.51172 -0.651483,0.38653 -1.368977,0.65986 -2.103515,0.87695 -1.573111,0.46493 -3.222106,0.67285 -4.861328,0.61133 a 0.1322915,0.1322915 0 0 0 -0.13672,0.12692 A 0.1322915,0.1322915 0 0 0 44,216.54297 c 1.668104,0.0626 3.346438,-0.14797 4.947266,-0.6211 0.747542,-0.22093 1.486426,-0.50224 2.164062,-0.90429 0.67799,-0.40226 1.294166,-0.92985 1.740234,-1.58985 0.65388,-0.96747 0.903959,-2.20423 0.634766,-3.34375 -0.18272,-0.77347 -0.597225,-1.4855 -1.171875,-2.0332 -0.574564,-0.54761 -1.303988,-0.92592 -2.080078,-1.08789 z"
+          id="path7472" />
+       <circle
+          style={{fill: isDark ? "#ffffff" : "#000000", fillOpacity: 1, fillRule: "evenodd", stroke: "none", strokeWidth: 0.999989, strokeLinecap: "square", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "0.0999999, 0.0999999", strokeDashoffset: 1.80001, strokeOpacity: 1, paintOrder: "stroke markers fill"}}
+          id="path7578"
+          cx="47.896324"
+          cy="209.69534"
+          r="0.26458332" />
+         <ellipse
+            style={{fill: isDark ? "#ffffff" : "#000000", fillOpacity: 1, fillRule: "evenodd", stroke: "none", strokeWidth: 1.00013, strokeLinecap: "square", strokeLinejoin: "round", strokeMiterlimit: 4, strokeDasharray: "0.100013, 0.100013", strokeDashoffset: 1.80024, strokeOpacity: 1, paintOrder: "stroke markers fill"}}
+            id="path7582"
+            cx="49.926418"
+            cy="209.67255"
+            rx="0.26458332"
+            ry="0.26458335" />
+         <path
+            style={{color: isDark ? "#ffffff" : "#000000", fill: isDark ? "#ffffff" : "#000000", strokeLinecap: "round"}}
+            d="m 50.894531,211.31445 a 0.1322915,0.1322915 0 0 0 -0.169922,0.0762 c -0.01911,0.0506 -0.041,0.10168 -0.06445,0.1504 -0.146804,0.30499 -0.379821,0.56815 -0.664062,0.75195 -0.284242,0.1838 -0.620607,0.28814 -0.958985,0.29687 -0.338377,0.009 -0.68126,-0.0772 -0.974609,-0.24609 -0.293349,-0.16889 -0.540774,-0.42173 -0.703125,-0.71875 a 0.1322915,0.1322915 0 0 0 -0.179688,-0.0527 0.1322915,0.1322915 0 0 0 -0.05273,0.17968 c 0.186103,0.34047 0.466468,0.62672 0.802734,0.82032 0.336267,0.19359 0.725399,0.29322 1.113282,0.2832 0.387883,-0.01 0.769877,-0.12915 1.095703,-0.33985 0.325826,-0.21069 0.591483,-0.5117 0.759765,-0.86132 0.02679,-0.0557 0.05239,-0.11213 0.07422,-0.16993 a 0.1322915,0.1322915 0 0 0 -0.07813,-0.16992 z"
+            id="path7699" />
+     </g>
+   </svg>;
+}
+
+export default Logo;
