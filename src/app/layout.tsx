c08c3e9ff2c6b580133c133ef3882abc1bfb22d7
asr <asr12211@outlook.com>
2023-09-10 11:36:23 +0300

    FIX: fonts added  correctly

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,20 @@
 import './globals.css'
 import type { Metadata } from 'next'
+import { Oregano, Laila } from 'next/font/google';
 
 // import AuthProvider from "../providers/supabaseProvider"; //! check if needed later
 
+const oregano = Oregano({
+  subsets: ['latin'],
+  weight: "400",
+  variable: "--font-header",
+});
+
+const laila = Laila({
+  subsets: ['latin'],
+  weight: ["400", "600"],
+  variable: "--font-body",
+});
 
 export const metadata: Metadata = {
   title: 'Chatty',
@@ -15,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${oregano.variable} ${laila.variable}`}>
       <body>
         {/* <AuthProvider> */}
           {children}
