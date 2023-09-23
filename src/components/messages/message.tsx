dd64f534ada88270bc559ceb2005c6de327ff96a
asr <asr12211@outlook.com>
2023-09-23 15:45:19 +0300

    EDIT: images in msgs are now clickable

--- src/components/messages/message.tsx
+++ src/components/messages/message.tsx
@@ -1,8 +1,11 @@
-import Image from "next/image";
+"use client";
 
-export default function Message({
-   id, body, file_url, time, read_status, sender_id, userId, isEnd
-}: {
+import Image from "next/image";
+import { useState } from "react";
+
+import ImageModal from "../imageModal/imageModal";
+
+type Props = {
    id: string,
    body: string | null,
    file_url: string | null,
@@ -11,14 +14,19 @@ export default function Message({
    sender_id: string,
    userId: string,
    isEnd: boolean
-}) {
+}
+
+export default function Message({ id, body, file_url, time, read_status, sender_id, userId, isEnd }: Props) {
+   
+   const [isOpen, setIsOpen] = useState(false);
 
    return <div className={`${sender_id === userId ? "float-right bg-msg-own-bg-color" : "float-left bg-msg-other-bg-color"} 
-      min-w-[15%] max-w-[70%] border-[1px] border-black rounded-lg overflow-hidden
-      ${isEnd && sender_id === userId ? "rounded-ee-none" : isEnd && sender_id !== userId ? "rounded-se-none" : null}`}
+      min-w-[15%] border-[1px] border-black rounded-lg overflow-hidden
+      ${isEnd && sender_id === userId ? "rounded-ee-none" : isEnd && sender_id !== userId ? "rounded-se-none" : null}
+      ${file_url ? "max-w-[65vw] md:max-w-[35vw] lg:max-w-[45vw]" : "max-w-[70%]"}`}
    >
       {
-         !file_url ? null : <div>
+         !file_url ? null : <div role="button" onClick={() => setIsOpen(true)}>
             <Image
                src={file_url}
                alt="message image"
@@ -26,6 +34,7 @@ export default function Message({
                height={0}
                sizes="100vw"
                style={{ width: "100%", height: "100%" }}
+               className="max-w-[65vw] md:max-w-[35vw] lg:max-w-[45vw]"
             />
          </div>
       }
@@ -45,5 +54,11 @@ export default function Message({
             sender_id === userId && <p className="ml-auto font-sans">{read_status ? "seen" : "sent"}</p>
          }
       </div>
+
+      <ImageModal
+         imgSrc={file_url ?? ""}
+         isOpen={isOpen}
+         setIsOpen={setIsOpen}
+      />
    </div>
 }
