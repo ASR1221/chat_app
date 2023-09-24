863b56af423889f3a1572b959e62cb7ef1211a60
asr <asr12211@outlook.com>
2023-09-24 20:55:51 +0300

    FIX: userListItem isOwner check fixed

--- src/components/userListItem/userListItem.tsx
+++ src/components/userListItem/userListItem.tsx
@@ -10,7 +10,6 @@ type Props = {
 
 export default function UserListItem({ id, profile_img_url, user_name, full_name, isOwner}: Props) {
 
-
    return <Link href={`/user/profile/${id}`} className="grid grid-cols-[16%_80%] gap-5">
       <div className="rounded-md overflow-hidden aspect-square bg-devider-line-color min-w-[40px] max-w-[55px]">
          {profile_img_url && <img src={profile_img_url} alt={`${user_name} profile image`} className="w-[100%] aspect-square object-cover" />}
@@ -19,7 +18,7 @@ export default function UserListItem({ id, profile_img_url, user_name, full_name
       <div className="grid grid-cols-1">
          <div className="flex items-center gap-6">
             <p className="text-lg font-semibold">{user_name}</p>
-            {typeof isOwner !== undefined && <p className="text-devider-line-color text-sm">{isOwner ? "Owner" : "Member"}</p>}
+            {typeof isOwner !== "undefined" && <p className="text-devider-line-color text-sm">{isOwner ? "Owner" : "Member"}</p>}
          </div>
          <p className="text-sm">{full_name}</p>
       </div>
