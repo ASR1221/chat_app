a5c562f59843319840fb760d48daef83d8d50116
asr <asr12211@outlook.com>
2023-09-25 22:29:54 +0300

    ADD: search page done

--- src/app/(user_layout)/user/search/page.tsx
+++ src/app/(user_layout)/user/search/page.tsx
@@ -1,29 +1,49 @@
 "use client";
 
 import Link from "next/link";
-import { useSearchParams } from "next/navigation"
-import { useEffect, useState } from "react";
+import { useRouter, useSearchParams } from "next/navigation"
+import { useEffect, useState, type BaseSyntheticEvent } from "react";
 
 import { useRealtime } from "@/providers/realtimeProvider";
+import useTheme from "@/hooks/useTheme";
 import { clientSupabase } from "@/utils/clientSupabase";
 import { User } from "@/types/supabaseTables";
 
+import SimpleNav from "@/components/simpleNav/simpleNav";
+import UserListItem from "@/components/userListItem/userListItem";
+import ConvoListItem from "@/components/convoListItem/convoListItem";
+import EmptyList from "@/components/emptyList/emptyList";
+
 export default function Search() {
 
-   const { convos, conts } = useRealtime();
+   const { convos, conts, userId } = useRealtime();
+   const { isDark } = useTheme();
 
    const searchTerm = useSearchParams().get("term");
+   const { push } = useRouter();
 
+   const [inputValue, setInputVlaue] = useState("");
    const [users, setUsers] = useState<Omit<User, "created_at">[] | null>(null);
-   const [conversations, setConversations] = useState<typeof convos>();
+   const [conversations, setConversations] = useState<typeof convos | null>(null);
    const [isError, setIsError] = useState(false);
-   const [contacts, setContacts] = useState(conts);
+   const [isLoading, setIsLoading] = useState(false);
+
+   function handleSubmit(e: BaseSyntheticEvent) {
+      e.preventDefault();
+      push(`/user/search?term=${inputValue.trim()}`);
+   }
 
    useEffect(() => {
 
+      setInputVlaue(searchTerm ?? "");
+      setUsers(null);
+      setConversations(null);
+      setIsError(false);
+
       if (!searchTerm) return;
 
-      if (searchTerm.includes("@")) {
+      if (searchTerm.startsWith("@")) {
+         setIsLoading(true);
          clientSupabase.from("users")
             .select("bio, full_name, id, last_seen, profile_img_url, user_name")
             .eq("user_name", searchTerm.split("@")[1])
@@ -34,50 +54,65 @@ export default function Search() {
                }
                setUsers(response.data);
             });
-      } else {
-         const convResult = convos?.filter(conv => conv.name === searchTerm);
-         setConversations(convResult);
-   
-         const contsResult = conts?.filter(cont => cont.users[0].full_name === searchTerm || cont.users[0].user_name === searchTerm);
-         setContacts(contsResult);
-      }
+         
+         setIsLoading(false);
+         return;
+      } 
+
+      const convResult = convos?.filter(conv => conv.name.includes(searchTerm));
+      setConversations(convResult);
 
    }, [searchTerm]);
 
-   return <div>
-      {
-         !users && contacts && contacts.length > 0 && <div>
-            {
-               contacts.map(cont => <Link href={`/user/contacts/${cont.users[0].id}`} key={cont.users[0].id}>
-                  <div>{cont.users[0].profile_img_url && <img src={cont.users[0].profile_img_url} alt={`${cont.users[0].full_name} profile image`} />}</div>
-                  <p>{cont.users[0].full_name}</p>
-               </Link>)
-            }
-         </div>
-      }
-      {
-         users && users.length > 0 && <div>
-            {
-               users.map(user => <Link href={`/user/profile/${user.id}`} key={user.id}>
-                  <div>{user.profile_img_url && <img src={user.profile_img_url} alt={`${user.full_name} profile image`} />}</div>
-                  <p>{user.full_name}</p>
-                  <p>{user.user_name}</p>
-               </Link>)
-            }
-         </div>
-      }
-      {
-         !users && conversations && conversations.length > 0 && <div>
-            {
-               conversations.map(conversation => <Link href={`/user/conversation/${conversation.id}`} key={conversation.id}>
-                  <div>{conversation.group_img_url && <img src={conversation.group_img_url} alt={`${conversation.name} group image`} />}</div>
-                  <p>{conversation.name}</p>
-               </Link>)
-            }
-         </div>
-      }
-      {
-         isError && <p>Something went wrong. Please refresh the page.</p>
-      }
+   return <div className="absolute top-0 bottom-0 bg-bg-color w-[100%] px-5 py-24">
+      <SimpleNav backPath="/user" />
+
+      <form onSubmit={handleSubmit}>
+         <input
+            type="text"
+            placeholder="Search..."
+            value={inputValue}
+            onChange={(e) => setInputVlaue(e.target.value.trim())}
+            className={`w-[100%] px-3 py-1 mb-8 rounded-md outline-none ${isDark ? "bg-gray-700/60" : "bg-gray-700/30"}`}
+         />
+      </form>
+
+      <div>
+         {
+            isError ? <div className="w-fit mx-auto mt-32">
+               <div className="w-[300px] mx-auto">
+                  <img src="/images/illustrations/500 Internal Server Error-amico.svg" alt="Empty chat illustration" />
+               </div>
+               <p>Something went wrong. Please refresh the page and try again.</p>
+            </div>
+            : !searchTerm && conts && conts.length > 0 ? <>
+               <h3 className="text-2xl">Contacts</h3>
+               <div className="grid grid-rows-1 overflow-x-scroll no-scrollbar p-2">
+                  {
+                     conts.map(cont => <Link href={`/user/profile/${cont.users.id}`} key={cont.users.id} className="p-2 w-fit">
+                        <div className="w-10 aspect-square bg-devider-line-color rounded-full">
+                           {cont.users.profile_img_url && <img src={cont.users.profile_img_url} alt={`${cont.users.user_name} profile image`} />}
+                        </div>
+                        <p className="w-10 overflow-hidden text-center text-sm">{cont.users.user_name}</p>
+                     </Link>)
+                  }
+               </div>
+            </> : users && users.length > 0 ? <div className="[&>*:not(:last-child)]:border-b-[1px] [&>*]:border-devider-line-color">
+               {
+                  users.map(user => <UserListItem
+                     key={user.id}
+                     id={user.id}
+                     profile_img_url={user.profile_img_url}
+                     user_name={user.user_name}
+                     full_name={user.full_name}
+                  />)
+               }
+            </div> : conversations && conversations.length > 0 ? <div className="[&>*:not(:last-child)]:border-b-[1px] [&>*]:border-devider-line-color">
+               {
+                  conversations.map(conversation => <ConvoListItem key={conversation.id} convo={conversation} userId={userId} />)
+               }
+            </div> : !isLoading && <EmptyList text="Nothing found. Make sure you write your search correctly." />
+         }
+      </div>
    </div>
 }
