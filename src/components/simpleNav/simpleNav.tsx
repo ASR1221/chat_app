41c767825a8dd326b4cf9249c9dd68ce7cbd850a
asr <asr12211@outlook.com>
2023-09-26 16:48:45 +0300

    EDIT: simpleNav header text added

--- src/components/simpleNav/simpleNav.tsx
+++ src/components/simpleNav/simpleNav.tsx
@@ -6,14 +6,14 @@ import useTheme from "@/hooks/useTheme";
 
 import ArrowIcon from "@/svgs/arrowIcon";
 
-export default function SimpleNav({ backPath }: { backPath: string }) {
+export default function SimpleNav({ backPath, header }: { backPath: string, header?: string }) {
 
    const { push } = useRouter();
    const pathname = usePathname();
-   const { isDark } = useTheme()
+   const { isDark } = useTheme();
 
    return <nav
-      className={`grid items-center px-4 fixed top-0 ${
+      className={`grid grid-cols-[1fr_4fr] items-center px-4 fixed top-0 ${
          pathname === "/user" ? "left-[100vw]" : "left-[0]"
       } w-[100vw] md:w-[calc(100vw-370px)] md:left-[370px] bg-convo-header-bg-color text-convo-header-text-color h-[72px] isolate`}
    >
@@ -21,6 +21,10 @@ export default function SimpleNav({ backPath }: { backPath: string }) {
       <button type="button" onClick={() => push(backPath)} className="pl-2">
          <ArrowIcon isDark={isDark} width={20} />
       </button>
+
+      {
+         header && <h2 className="text-3xl text-convo-header-text-color">{header}</h2>
+      }
 
    </nav>
 }
