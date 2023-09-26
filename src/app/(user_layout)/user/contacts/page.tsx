142230b3ca8a3e8c4a0316c86c389b1ae1bf0648
asr <asr12211@outlook.com>
2023-09-26 16:50:09 +0300

    ADD: search in contacts

--- src/app/(user_layout)/user/contacts/page.tsx
+++ src/app/(user_layout)/user/contacts/page.tsx
@@ -1,6 +1,9 @@
 "use client";
 
+import { BaseSyntheticEvent, useEffect, useState } from "react";
+
 import { useRealtime } from "@/providers/realtimeProvider";
+import useTheme from "@/hooks/useTheme";
 
 import UserListItem from "@/components/userListItem/userListItem";
 import SimpleNav from "@/components/simpleNav/simpleNav";
@@ -9,13 +12,35 @@ import EmptyList from "@/components/emptyList/emptyList";
 export default function Contacts() {
 
    const { conts } = useRealtime();
+   const { isDark } = useTheme();
 
-   return <div className="absolute top-0 bottom-0 bg-bg-color w-[100%] px-5 py-14">
-      <SimpleNav backPath="/user" />
-      <h2 className="text-3xl py-5">Contacts</h2>
+   const [currentConts, setCurrenConts] = useState<typeof conts | null>(null);
+
+   function handleChange(e: BaseSyntheticEvent) {
+      const text = e.target.value.trim();
+      if (!text) setCurrenConts(conts);
+      else setCurrenConts(conts.filter(c => c.users.full_name.includes(text) || c.users.user_name.includes(text)));
+   }
+
+   useEffect(() => {
+      setCurrenConts(conts)
+   }, [conts]);
+
+   return <div className="absolute top-0 bottom-0 bg-bg-color w-[100%] px-5 py-20">
+      <SimpleNav backPath="/user" header="Contacts" />
+
+      <form>
+         <input
+            type="text"
+            placeholder="Search..."
+            onChange={handleChange}
+            className={`w-[100%] px-3 py-1 mb-8 rounded-md outline-none ${isDark ? "bg-gray-700/60" : "bg-gray-700/30"}`}
+         />
+      </form>
+
       {
-         conts?.length < 1 ? <EmptyList text="No Contacts yet. Add a contacts and it will be seen here." />
-         : conts?.flatMap(contact => {
+         currentConts && (currentConts.length < 1 ? <EmptyList text="No Contacts yet. Add a contacts and it will be seen here." />
+         : currentConts?.flatMap(contact => {
             
             const user = contact.users;
             
@@ -27,7 +52,7 @@ export default function Contacts() {
                   full_name={user.full_name}
                />
             </div>
-         })
+         }))
       }
    </div>
 }
