928d7d3d71113b55eab20552166a356aa7b8ef2d
asr <asr12211@outlook.com>
2023-10-02 22:21:40 +0300

    ADD: not found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+ 
+export default function NotFound() {
+   return <div className="w-fit mx-auto mt-32">
+      <div className="w-[300px] mx-auto">
+         <img src="/images/illustrations/404 Error Page not Found with people connecting a plug-bro.svg" alt="Not Found illustration" />
+      </div>
+      <p className="text-center w-[300px]">Page not found. Return to gome page?</p>
+      <div className="w-fit mx-auto">
+         <Link href="/" className="p-1 bg-btn-color rounded-md hover:bg-btn-border-color cursor-pointer text-white text-center transition-all duration-300">Return Home</Link>
+      </div>
+   </div>
+}
