816007399520531cef610b9216b092ce3cfc8438
asr <asr12211@outlook.com>
2023-10-05 15:11:34 +0300

    FIX: root layout is now a client comp

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,21 +1,19 @@
-"use client";
-
 import './globals.css';
 import type { Metadata } from 'next';
 import { Oregano, Laila } from 'next/font/google';
-
-import { useEffect } from 'react';
 
 const oregano = Oregano({
   subsets: ['latin'],
   weight: "400",
   variable: "--font-header",
+  preload: true,
 });
 
 const laila = Laila({
   subsets: ['latin'],
   weight: ["400", "600"],
   variable: "--font-body",
+  preload: true,
 });
 
 export const metadata: Metadata = {
@@ -24,30 +22,6 @@ export const metadata: Metadata = {
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  
-  useEffect(() => {
-    let theme: "light" | "dark" = "light";
-
-    if (localStorage && localStorage.getItem("theme")) {
-      theme = localStorage.getItem("theme") as "light" | "dark";
-    } else if (!window.matchMedia) {
-      theme = "light";
-    } else if (window.matchMedia("(prefers-color-scheme: dark)").matches){
-      theme = "dark";
-    }
-
-    document.documentElement.setAttribute("data-theme", theme);
-
-    function setTheme(isDark: boolean) {
-      if (!(localStorage && localStorage.getItem("theme")))
-        document.documentElement.setAttribute("data-theme", isDark ? "dark" : "light");
-    }
-
-    window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", e => setTheme(e.matches));
-
-    return () => window.matchMedia("(prefers-color-scheme: dark)").removeEventListener("change", e => setTheme(e.matches));
-
-  }, []);
 
   return (
     <html lang="en" className={`${oregano.variable} ${laila.variable}`}>
