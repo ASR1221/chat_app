efc8b247493d891e069bbeea3481e04b99ded970
asr <asr12211@outlook.com>
2023-10-18 19:09:18 +0300

    EDIT: not-found style edit

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -6,7 +6,7 @@ export default function NotFound() {
          <img src="/images/illustrations/404 Error Page not Found with people connecting a plug-bro.svg" alt="Not Found illustration" />
       </div>
       <p className="text-center w-[300px]">Page not found. Return to gome page?</p>
-      <div className="w-fit mx-auto">
+      <div className="w-fit mx-auto my-5">
          <Link href="/" className="p-1 bg-btn-color rounded-md hover:bg-btn-border-color cursor-pointer text-white text-center transition-all duration-300">Return Home</Link>
       </div>
    </div>
